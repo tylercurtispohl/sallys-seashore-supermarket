2b1fe4425dac82feb75ddd36dfebe89ca12c13f6
Tyler Pohl <tylercurtispohl@gmail.com>
2023-07-08 13:22:10 -0700

    simple product creation in productCrud lambda

--- amplify/backend/function/productCrud/src/index.js
+++ amplify/backend/function/productCrud/src/index.js
@@ -4,20 +4,52 @@
 	STORAGE_SALLYDB_ARN
 	STORAGE_SALLYDB_NAME
 	STORAGE_SALLYDB_STREAMARN
-Amplify Params - DO NOT EDIT *//**
+Amplify Params - DO NOT EDIT */
+
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require('uuid');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+/**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  if (event.requestContext.authorizer) {
-    console.log(`CLAIMS: `, event.requestContext.authorizer.claims);
+  
+  switch(event.httpMethod) {
+    case("POST"): 
+        const body = JSON.parse(event.body);
+        const newItem = {
+            ...body,
+            id: uuidv4()
+        }
+
+        console.log(`NEW PRODUCT: ${JSON.stringify(newItem)}`);
+
+        docClient.put({
+            TableName: "products-main",
+            Item: newItem
+        });
+
+        return {
+            statusCode: 200,
+            headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify(newItem),
+        };
+    case("GET"):
+    default:
+        return {
+            statusCode: 200,
+            headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify("Hello from Lambda!"),
+        };
   }
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-    },
-    body: JSON.stringify("Hello from Lambda!"),
-  };
+  
+  
 };
