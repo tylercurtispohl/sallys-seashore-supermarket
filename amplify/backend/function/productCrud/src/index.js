cd225bc617d159793bf909d6169e7e4d213bc496
Tyler Pohl <tylercurtispohl@gmail.com>
2023-07-08 14:36:45 -0700

    separate put code out into function

--- amplify/backend/function/productCrud/src/index.js
+++ amplify/backend/function/productCrud/src/index.js
@@ -7,49 +7,64 @@
 Amplify Params - DO NOT EDIT */
 
 const AWS = require("aws-sdk");
-const { v4: uuidv4 } = require('uuid');
-const docClient = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" });
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  
-  switch(event.httpMethod) {
-    case("POST"): 
-        const body = JSON.parse(event.body);
-        const newItem = {
-            ...body,
-            id: uuidv4()
-        }
 
-        console.log(`NEW PRODUCT: ${JSON.stringify(newItem)}`);
+  const partialResponse = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+  };
 
-        await docClient.put({
-            TableName: "products-main",
-            Item: newItem
-        });
+  switch (event.httpMethod) {
+    case "POST":
+    case "PUT":
+      const body = JSON.parse(event.body);
 
-        return {
-            statusCode: 200,
-            headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*",
-            },
-            body: JSON.stringify(newItem),
-        };
-    case("GET"):
+      const result = await putProduct(body);
+
+      return {
+        ...partialResponse,
+        body: JSON.stringify(result),
+      };
+    case "GET":
     default:
-        return {
-            statusCode: 200,
-            headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*",
-            },
-            body: JSON.stringify("Hello from Lambda!"),
-        };
+      return {
+        ...partialResponse,
+        body: JSON.stringify("Hello from Lambda!"),
+      };
   }
-  
-  
+};
+
+const putProduct = async (productData) => {
+  const putItem = {
+    ...productData,
+    id: productData.id || uuidv4(),
+  };
+
+  console.log(`PUTTING PRODUCT: ${JSON.stringify(newItem)}`);
+
+  try {
+    const result = await docClient
+      .put({
+        TableName: "products-main",
+        Item: putItem,
+      })
+      .promise();
+
+    console.log(`PUT RESULT: ${JSON.stringify(result)}`);
+
+    return result;
+  } catch (error) {
+    console.log(`ERROR PUTTING PRODUCT: ${error}`);
+    throw error;
+  }
 };
