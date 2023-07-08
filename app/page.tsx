c54a50d67f11ff4ae56852d0a386236f48fb56eb
Tyler Pohl <tylercurtispohl@gmail.com>
2023-07-08 11:10:44 -0700

    Add authenticator to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+"use client";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify, API, Auth } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import awsconfig from "../src/aws-exports";
+import Button from "@mui/material/Button";
 
-export default function Home() {
+Amplify.configure({ ...awsExports, ssr: true });
+Auth.configure(awsconfig);
+
+const withAuthenticatorOptions = {
+  hideSignUp: false,
+};
+
+export default withAuthenticator(function Home({ signOut, user }) {
   return (
     <main className="">
       <h1>HOME</h1>
+      <h1>Logged in as {user?.username}.</h1>
+      <div>
+        <Button onClick={signOut}>Sign Out</Button>
+      </div>
     </main>
   );
-}
+}, withAuthenticatorOptions);
