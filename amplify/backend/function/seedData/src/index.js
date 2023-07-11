5484810ed798275ffc913b09ff0adf2155efbaec
Tyler Pohl <tylercurtispohl@gmail.com>
2023-07-10 18:29:20 -0700

    forgot price on product

--- amplify/backend/function/seedData/src/index.js
+++ amplify/backend/function/seedData/src/index.js
@@ -57,6 +57,7 @@ exports.handler = async (event) => {
       imageList[randomIntFromInterval(0, imageList.length - 1)];
     const randomNumber = randomIntFromInterval(1, 999999999);
     const randomQuantity = randomIntFromInterval(0, 1000);
+    const randomPrice = randomIntFromInterval(1, 100) + 0.99;
 
     const putItem = {
       id: uuidv4(),
@@ -65,6 +66,7 @@ exports.handler = async (event) => {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       imageKey: randomImageKey,
       stockQuantity: randomQuantity,
+      price: randomPrice,
       createdAt: now,
       updatedAt: now,
     };
