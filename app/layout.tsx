d8acbc82d47f1fd8b7cd0a95d08b7b3fef0878f0
Tyler Pohl <tylercurtispohl@gmail.com>
2023-07-10 21:34:21 -0700

    override material ui primary color to cyan

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
+"use client";
 import Nav from "@/components/Nav";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { cyan } from "@mui/material/colors";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Sally's Seashore Supermaket",
-  description: "Way more than seashells",
-};
+// override the primary color to match the cyan logo
+const theme = createTheme({
+  palette: {
+    primary: cyan,
+  },
+});
 
 export default function RootLayout({
   children,
@@ -16,15 +21,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <main className="p-5 flex flex-row justify-around">
-          <div className="xl:w-2/3 w-full lg:w-3/4 ">
-            <Nav />
-            <div className="mt-5 px-10 lg:px-0">{children}</div>
-          </div>
-        </main>
-      </body>
-    </html>
+    <ThemeProvider theme={theme}>
+      <html lang="en">
+        <head>
+          <title>Sally&apos;s Seashore Supermarket</title>
+        </head>
+        <body className={inter.className}>
+          <main className="p-5 flex flex-row justify-around">
+            <div className="xl:w-2/3 w-full lg:w-3/4 ">
+              <Nav />
+              <div className="mt-5 px-10 lg:px-0">{children}</div>
+            </div>
+          </main>
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
